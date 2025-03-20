e93712465d994c4c3f47f8f68d8e319769a09d86
wahyudianpratiwi1 <dian@andalsoftware.com>
2025-03-20 08:28:36 +0700

    Auto-generated commit

---
+++ chunk-PYK4TH6B.js
@@ -0,0 +1,1 @@
+import{Oa as o,Pa as a,W as n,Za as c,eb as r}from"./chunk-E2B3W7VT.js";var s=(()=>{class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-contact"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),c(1,"contact works!"),a())}})}return t})();export{s as ContactComponent};
