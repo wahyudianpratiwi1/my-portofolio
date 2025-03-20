f89185e8065794e0519b4d680877bc185a500f92
wahyudianpratiwi1 <dian@andalsoftware.com>
2025-03-20 08:54:06 +0700

    Auto-generated commit

--- server/chunk-4IC54UPC.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Oa as o,Pa as a,U as n,Za as c,eb as r}from"./chunk-SMCTTTOZ.mjs";import"./chunk-5XUXGTUW.mjs";var s=(()=>{class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-contact"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,p){e&1&&(o(0,"p"),c(1,"contact works!"),a())}})}return t})();export{s as ContactComponent};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ZEGIMNSS.mjs";import"./chunk-X76W746I.mjs";import"./chunk-LV4EQ3J4.mjs";import"./chunk-SMCTTTOZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
